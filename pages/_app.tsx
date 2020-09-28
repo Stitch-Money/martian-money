e786bcd1973f07cfc5993ae57c581e37c97a35b6
james <james@stitch.money>
2020-09-28 14:32:14 +0200

    Add bulma

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/global.css'
 import React from "react"
+import 'bulma/css/bulma.min.css';
 
 export default function App({Component, pageProps }: { Component: React.FC, pageProps: any }) {
   return <Component {...pageProps} />
