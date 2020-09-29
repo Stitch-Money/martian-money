6cbdf7539dd83a147618ecef2610acdff5ed2d04
james <james@stitch.money>
2020-09-29 09:58:35 +0200

    typo

--- pages/about/about.tsx
+++ pages/about/about.tsx
@@ -6,7 +6,7 @@ export default function About() {
     <section>
       <div className="container">
         <h1 className="title">New Horizons</h1>
-        <p className="content">Olympus Mons is calling, and the great migrations has begun.
+        <p className="content">Olympus Mons is calling, and the great migration has begun.
           It's time to leave behind our earthly possessions and launch ourselves at new horizons!
           The team at <strong>{ siteName }</strong> will help you transfer all your wealth to your new home on Mars, <strong>the mightiest of the planets!</strong>
         </p>
