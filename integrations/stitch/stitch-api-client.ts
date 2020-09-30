efa352b3278e1fd9a26442a40e096a17e3e91822
Marcel Prinsloo <marcel@stitch.money>
2020-09-30 14:24:20 +0200

    Add the StitchProvider component.

--- integrations/stitch/stitch-api-client.ts
+++ integrations/stitch/stitch-api-client.ts
@@ -1,38 +1,27 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { BankAccounts } from './query/queries';
-import { getStitchAccessToken } from '../storage/session-storage';
 
 // Apollo Client chains together ApolloLinks to add our token to every request
 
-const httpLink = createHttpLink({
-    uri: 'https://api.stitch.money/graphql'
-});
-
-const authLink = setContext((_, { headers }) => {
-    const token = getStitchAccessToken();
-
-    if (token === null) {
-        throw new Error('Token was undefined. Failed to create ApolloClient auth link.');
-    }
-
-    return {
-        headers: {
-            ...headers,
-            authorization: `Bearer ${token}`
+export const stitchClient = (token: string) => {
+    const httpLink = createHttpLink({
+        uri: 'https://api.stitch.money/graphql'
+    });
+    const authLink = setContext((_, { headers }) => {
+        if (token === null) {
+            throw new Error('Token was undefined. Failed to create ApolloClient auth link.');
         }
-    };
-});
 
-const stitchClient = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
-});
+        return {
+            headers: {
+                ...headers,
+                authorization: `Bearer ${token}`
+            }
+        };
+    });
 
-export async function getBankAccounts() {
-    const result = await stitchClient.query({ query: BankAccounts });
-
-    console.log('Result from Stitch API.\nData: ', result.data, '\nErrors:', result.errors);
-
-    return result.data;
-}
+    return new ApolloClient({
+        link: authLink.concat(httpLink),
+        cache: new InMemoryCache()
+    });
+};
--- pages/report/index.tsx
+++ pages/report/index.tsx
@@ -1,55 +1,11 @@
 import React from 'react';
-import useSWR from 'swr';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import Layout, { siteName } from 'components/layout';
-import { IncomeExpenseChart } from 'components/report/incomeExpenseChart';
-import { TransactionCategoryChart } from 'components/report/transactionCategoryChart';
-import { retrieveTokenUsingAuthorizationCode } from 'integrations/stitch/fetch-token';
-import { getSessionVerifier } from "integrations/storage/session-storage";
-
-function getAccessToken() {
-    const router = useRouter();
-    const { code } = router.query;
-    const verifier = getSessionVerifier();
-
-    return useSWR([code, verifier], retrieveTokenUsingAuthorizationCode);
-}
+import { StitchProvider } from 'components/stitch/stitch-provider';
+import { ReportContents } from '../../components/report/reportContents';
 
 export default function Report(): JSX.Element {
-    const { data, error } = getAccessToken();
-    if (data) {
-        return (
-            <Layout>
-                <Head>
-                    <title>Report | {siteName}</title>
-                </Head>
-                <section className="section">
-                    <div className="container">
-                        <h1 className="title">Congrats! You&apos;re on your way!</h1>
-                        <div className="tile is-ancestor">
-                            <IncomeExpenseChart/>
-                            <TransactionCategoryChart/>
-                        </div>
-                    </div>
-                </section>
-            </Layout>
-        );
-    } else {
-        console.log(error);
-        return (
-            <Layout>
-                <Head>
-                    <title>Report | {siteName}</title>
-                </Head>
-                <section className="section">
-                    <div className="container">
-                        <h1 className="title">Catastrophy has struck!</h1>
-                    </div>
-                </section>
-            </Layout>
-        );
-    }
+    return <StitchProvider>
+        <ReportContents/>
+    </StitchProvider>;
 }
 
 export async function getStaticProps() {
