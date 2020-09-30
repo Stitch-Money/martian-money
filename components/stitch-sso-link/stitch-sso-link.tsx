98146f522e5bc81fcd3816e7ea563a2cc660b3f4
Jacques Lewis <jacques@techvillage.co.za>
2020-09-30 14:24:56 +0200

    Rounded the apply button

--- components/stitch-sso-link/stitch-sso-link.tsx
+++ components/stitch-sso-link/stitch-sso-link.tsx
@@ -2,6 +2,7 @@ import { getStitchAuthorizationCodeUrl } from 'integrations/stitch/authorize-use
 import useSWR from 'swr';
 import React from 'react';
 import Link from 'next/link';
+import styles from './stitch-sso-link.module.css';
 
 export default function StitchSSOLink() {
     const { data } = useSWR('stitchUrl', getStitchAuthorizationCodeUrl);
@@ -9,13 +10,13 @@ export default function StitchSSOLink() {
     if (data != null) {
         return (
             <Link href={ data }>
-                <a className="button is-large is-primary">Apply now</a>
+                <a className={styles.round.concat(' button is-large is-primary')}>Apply now</a>
             </Link>
         );
     }
     return (
         <Link href="/">
-            <a className="button is-large is-primary is-disabled">Apply now</a>
+            <a className="button is-large is-primary is-disabled round">Apply now</a>
         </Link>
     );
 }
