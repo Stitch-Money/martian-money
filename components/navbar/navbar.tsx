56015468e8644cbdfd237ddd8f62406d99df1f48
Jacques Lewis <jacques@techvillage.co.za>
2020-10-01 08:16:45 +0200

    Added mars financial logo

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -5,7 +5,9 @@ export default function Navbar() {
         <nav id="navbar" className={'navbar is-spaced is-center'}>
             <div className="column">
                 <div className="level-item is-center">
-                    <img className="is-center" src={'/favicon.svg'} width={'60px'} alt=""/>
+                    <div className="col">
+                        <img className="is-center" src={'images/mars-logo.svg'} width={'250px'} alt=""/>
+                    </div>
                 </div>
             </div>
         </nav>
