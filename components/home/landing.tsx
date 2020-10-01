83aa7d70652c9b0f6208662b780d352cbea626cd
Jacques Lewis <jacques@techvillage.co.za>
2020-10-01 09:25:10 +0200

    Added the dome, css sucks

--- components/home/landing.tsx
+++ components/home/landing.tsx
@@ -4,6 +4,9 @@ import StitchSSOLink from '../stitch-sso-link/stitch-sso-link';
 export default function Landing() {
     return (
         <section className="hero">
+            <div>
+                <img  className='dome' src='images/dome.svg' alt=''/>
+            </div>
             <div className="hero-body">
                 <div className="column">
                     <div className="level-item is-center">
