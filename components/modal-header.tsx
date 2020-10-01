b67263f33a283b3b7cc8e121abcd4dd74238b3d3
Marcel Prinsloo <marcel@stitch.money>
2020-10-01 17:09:28 +0200

    Modal pages.

---
+++ components/modal-header.tsx
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
+import React, { MouseEventHandler } from 'react';
+
+export function ModalHeader(props: { onClose: MouseEventHandler }) {
+    return <nav id="navbar" className={'navbar is-spaced'}>
+        <div className="navbar-menu">
+            <div className="navbar-end">
+                <button className="button is-link is-inverted" onClick={props.onClose}>
+                    <span className="icon is-small">
+                        <FontAwesomeIcon icon={faTimes}/>
+                    </span></button>
+            </div>
+        </div>
+    </nav>;
+}
