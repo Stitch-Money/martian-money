11fd55ec1c01c390681f6468460fe6b77790c854
Marcel Prinsloo <marcel@stitch.money>
2020-10-02 09:53:45 +0200

    Fix footer on mobile.

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,11 +4,11 @@ export default function Footer(props: { onReadMoreClick: () => void }): JSX.Elem
     return (
         <footer className={'footer stitch-footer'}>
             <div className="content has-text-centered">
-                <div className='columns'>
-                    <div className='column is-2 is-offset-4 has-text-right has-text-grey'>
+                <div className='columns is-mobile'>
+                    <div className='column is-2-desktop is-offset-4-desktop has-text-right has-text-grey'>
                         A <strong className="has-text-grey">demo</strong> by Stitch
                     </div>
-                    <div className='column is-2 has-text-left stitch-footer-text-subtle'>
+                    <div className='column is-2-desktop has-text-left stitch-footer-text-subtle'>
                         <a className='stitch-footer-text-orange' href="#" onClick={props.onReadMoreClick}>
                             Learn more
                         </a> | <a className='stitch-footer-text-orange' href="https://github.com/Stitch-Money/Mars-Capital">
