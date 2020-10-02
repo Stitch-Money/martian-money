402e89f4f274f3e0ce465d6358b97053b2c1d6bf
james <james@stitch.money>
2020-10-02 09:52:49 +0200

    Add button click handler

--- components/buttons/secondary-button.tsx
+++ components/buttons/secondary-button.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import { buttonClickHandler } from './button-click-handler';
 
 export function SecondaryButton(props: any & { href: string | undefined, children?: JSX.Element | JSX.Element[] | string }) {
     const [clicked, setClicked] = useState(false);
 
-    return <a { ...props } href={props.href} className={`button has-text-weight-bold is-medium is-info ${clicked ? 'is-loading' : ''}`} onClick={_ => { setClicked(true); props.onClick(); }}>{props.children}</a>;
+    return <a { ...props } href={props.href} className={`button has-text-weight-bold is-medium is-info ${clicked ? 'is-loading' : ''}`} onClick={_ => buttonClickHandler(setClicked, props.onClick)}>{props.children}</a>;
 }
