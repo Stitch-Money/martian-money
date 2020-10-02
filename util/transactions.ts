8b39156c30ff2358c0c4884f5a9e5441c5cd0c09
james <james@stitch.money>
2020-10-02 13:46:56 +0200

    Top 5 expenses

--- util/transactions.ts
+++ util/transactions.ts
@@ -1,7 +1,8 @@
 import { Transaction } from '../integrations/stitch/types';
 
-export function topFiveExpenses(transactions: Transaction[]): Transaction[] {
+export function topFiveExpensesThisMonth(transactions: Transaction[]): Transaction[] {
     const sorted = transactions
+        .filter(transaction => new Date(transaction.date).getMonth() === new Date().getMonth() - 1)
         .sort((a, b) => Number.parseFloat(a.amount.quantity) - Number.parseFloat(b.amount.quantity));
 
     console.log('Sorted:', sorted.map(x => x.amount.quantity));
