cbda832073c971fda33147aa1b16e89b27e3ac39
Jacques Lewis <jacques@techvillage.co.za>
2020-10-02 11:00:14 +0200

    Some work on income expense graph

--- components/report/incomeExpenseChart.tsx
+++ components/report/incomeExpenseChart.tsx
@@ -21,13 +21,13 @@ export function IncomeExpenseChart(): JSX.Element {
 
     return (
         <ResponsiveContainer className="tile" minHeight="300px">
-            <BarChart data={data} >
+            <BarChart data={data}>
                 <Tooltip cursor={false} formatter={valueFormatter} labelFormatter={getLabelFormatter}/>
-                <XAxis dataKey="month"/>
-                <YAxis tickFormatter={value => `R ${value}`} />
-                <Bar dataKey="income" fill="#cc7810"/>
-                <Bar dataKey="expenses" fill="#870D0D"/>
-                <Legend formatter={getLabelFormatter}/>
+                <XAxis dataKey="month" tick={{fontSize: 10}} axisLine={false} tickLine={false}/>
+                <YAxis tickFormatter={value => `R${value}`} tick={{fontSize: 10}} axisLine={false} tickLine={false} />
+                <Bar dataKey="income" fill="#D63908" barSize={12} />
+                <Bar dataKey="expenses" fill="#F87C55" barSize={12}/>
+                <Legend formatter={getLabelFormatter} height={15}/>
             </BarChart>
         </ResponsiveContainer>
     );
