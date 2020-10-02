d90e65966b412ce284331577d1014477ae920cd2
Marcel Prinsloo <marcel@stitch.money>
2020-10-02 14:22:28 +0200

    Idenity card rework.

--- components/report/identity.tsx
+++ components/report/identity.tsx
@@ -8,35 +8,36 @@ export function Identity(): JSX.Element {
     const identity = identityQuery.data?.user.identity;
     if (identity) {
         return (
-            <article>
-                <div className="columns">
-                    <div className="column">
-                        <div className="level-item is-center mb-6">
-                            <img src={` https://api.adorable.io/avatars/160/${encodeURIComponent(identity.fullName)}`}
+            <>
+                <article className="media is-align-items-center mb-0 mb-3">
+                    <figure className="media-left ml-0">
+                        <p className="image is-128x128">
+                            <img className="is-rounded"
+                                src={`https://api.adorable.io/avatars/160/${encodeURIComponent(identity.fullName)}`}
                                 alt="Placeholder image"/>
-                        </div>
-                        <div className="media-content">
-                            <div className="content">
-                                <table className="table">
-                                    <tbody>
-                                        <IdentityTableRow heading="Full Name" value={identity.fullName} />
-                                        <IdentityTableRow heading="Date of Birth" value={identity.dateOfBirth} />
-                                        <IdentityTableRow heading="Email" value={identity.email} />
-                                    </tbody>
-                                </table>
-                            </div>
+                        </p>
+                    </figure>
+                    <div className="media-content">
+                        <div className="content has-text-weight-light is-large">
+                            <p>
+                                <span className="is-size-6">Earth Name</span><br/>
+                                <strong className="">{identity.fullName}</strong>
+                            </p>
                         </div>
                     </div>
-                </div>
-            </article>
+                </article>
+                <IdentityTableRow heading="Communicator" value={identity.contact?.phoneNumber}/>
+                <IdentityTableRow heading="Email" value={identity.email}/>
+                <IdentityTableRow heading="Identifier" value={identity.identifyingDocument?.number}/>
+            </>
         );
     }
     return <p>Error</p>;
 }
 
-function IdentityTableRow(props: { heading: string, value: string }): JSX.Element {
-    return <tr>
-        <th className="is-size-7">{props.heading}</th>
-        <td className="is-size-7">{props.value}</td>
-    </tr>;
+function IdentityTableRow(props: { heading: string, value: string | undefined }): JSX.Element {
+    return <div className="field">
+        <span className="is-block has-text-grey has-text-weight-light is-size-7">{props.heading}</span>
+        <span className="is-size-5 has-text-black">{props.value}</span>
+    </div>;
 }
