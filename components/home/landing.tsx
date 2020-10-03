9a5a9c52630f9b3d63feedd04815f0a0654953aa
james <james@stitch.money>
2020-10-03 13:11:49 +0200

    Fix dome mobile layout

--- components/home/landing.tsx
+++ components/home/landing.tsx
@@ -4,12 +4,11 @@ import ApplyButton from 'components/apply-button/apply-button';
 export default function Landing() {
     return (
         <section className="hero">
-            <div className="dome">
-                <div className="hero-body">
-                    <div className="column">
-                        <div className="level-item is-center">
-                            <ApplyButton />
-                        </div>
+            <div className="dome"></div>
+            <div className="hero-body">
+                <div className="column">
+                    <div className="level-item is-center">
+                        <ApplyButton />
                     </div>
                 </div>
             </div>
