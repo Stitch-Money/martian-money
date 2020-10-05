f736fe565ab5212a76ffa41a123ccbf555406f0e
james <james@stitch.money>
2020-10-05 08:34:15 +0200

    Fix footer wrap on mobile

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,17 +3,21 @@ import React from 'react';
 export default function Footer(props: { onReadMoreClick: () => void }): JSX.Element {
     return (
         <footer className={'footer stitch-footer'}>
-            <div className="content has-text-centered">
-                <div className='columns is-mobile'>
-                    <div className='column is-2-desktop is-offset-4-desktop has-text-right has-text-grey'>
+            <div className="content has-text-centered ">
+                <div className='level is-mobile'>
+                    <div className='level-item is-2-desktop is-offset-4-desktop has-text-right has-text-grey is-justify-content-flex-end'>
+                        <div className="content">
                         A <strong className="has-text-grey">demo</strong> by Stitch
+                        </div>
                     </div>
-                    <div className='column is-2-desktop has-text-left stitch-footer-text-subtle'>
-                        <a className='stitch-footer-text-orange' href="#" onClick={props.onReadMoreClick}>
+                    <div className='level-item is-2-desktop has-text-left stitch-footer-text-subtle is-justify-content-flex-start'>
+                        <div className="content">
+                            <a className='stitch-footer-text-orange' href="#" onClick={props.onReadMoreClick}>
                             Learn more
-                        </a> | <a className='stitch-footer-text-orange' href="https://github.com/Stitch-Money/Mars-Capital">
+                            </a><span> | </span><a className='stitch-footer-text-orange' href="https://github.com/Stitch-Money/Mars-Capital">
                             Github
-                        </a>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
