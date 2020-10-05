47ba16a31d9948986e0f21b773879cb02e16b716
james <james@stitch.money>
2020-10-05 08:10:15 +0200

    Make buttons rounded

--- components/buttons/primary-button.tsx
+++ components/buttons/primary-button.tsx
@@ -4,5 +4,5 @@ import { buttonClickHandler } from './button-click-handler';
 export function PrimaryButton(props: any & { href: string | undefined, children?: JSX.Element | JSX.Element[] | string }) {
     const [clicked, setClicked] = useState(false);
 
-    return <a { ...props } href={props.href} className={`button has-text-weight-bold is-medium is-primary ${clicked ? 'is-loading' : ''}`} onClick={_ => buttonClickHandler(setClicked, props.onClick)}>{props.children}</a>;
+    return <a { ...props } href={props.href} className={`button is-rounded has-text-weight-bold is-medium is-primary ${clicked ? 'is-loading' : ''}`} onClick={_ => buttonClickHandler(setClicked, props.onClick)}>{props.children}</a>;
 }
--- components/buttons/secondary-button.tsx
+++ components/buttons/secondary-button.tsx
@@ -4,5 +4,5 @@ import { buttonClickHandler } from './button-click-handler';
 export function SecondaryButton(props: any & { href: string | undefined, children?: JSX.Element | JSX.Element[] | string }) {
     const [clicked, setClicked] = useState(false);
 
-    return <a { ...props } href={props.href} className={`button has-text-weight-bold is-medium is-info ${clicked ? 'is-loading' : ''}`} onClick={_ => buttonClickHandler(setClicked, props.onClick)}>{props.children}</a>;
+    return <a { ...props } href={props.href} className={`button is-rounded has-text-weight-bold is-medium is-info ${clicked ? 'is-loading' : ''}`} onClick={_ => buttonClickHandler(setClicked, props.onClick)}>{props.children}</a>;
 }
