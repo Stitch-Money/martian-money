882475b7878a4a8e8e52c5a2f0fbac007a952382
james <james@stitch.money>
2020-10-10 21:00:02 +0200

    Fix routing

--- pages/return/index.tsx
+++ pages/return/index.tsx
@@ -32,8 +32,6 @@ export default function Index() {
 
             if (token) {
                 router.push('/report').then(_ => {}, _ => {});
-            } else {
-                router.push('/').then(_ => {}, _ => {});
             }
         }, () => {});
     }, [router, code, verifier]);
