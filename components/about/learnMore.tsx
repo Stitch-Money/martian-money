b1ede060dd23d879f502f937723a815f7549ab31
Marcel Prinsloo <marcel@stitch.money>
2020-10-23 13:48:52 +0200

    Add revised copy to Learn More page.

--- components/about/learnMore.tsx
+++ components/about/learnMore.tsx
@@ -2,23 +2,18 @@ import React from 'react';
 import { PrimaryButton } from '../buttons/primary-button';
 import { ExplainerLayout } from '../explainer/explainer-layout';
 
-export function About(props: { onClose: () => void }) {
+export function LearnMore(props: { onClose: () => void }) {
     return (
         <ExplainerLayout onCloseClick={props.onClose}>
-            <div className="content has-text-weight-bold is-medium">Mars.Financial is a fictitious <a
-                href="#">demo website</a> showing how a
-                fintech company can use Stitch to link a user&apos;s African bank account to their
-                service
+            <div className="content has-text-weight-bold is-medium">Mars Financial uses technology provided by Stitch to
+                access your financial information in order to approve your dome loan.
             </div>
             <div className="content has-text-weight-light is-medium">
-                <p>It is 100% safe and you will not be debited any real money when you apply for a fake
-                    dome-loan on Mars :)</p>
-                <p>You can either use the demo with example credentials we provide or with you own
-                    information.</p>
+                <p>Stitch gives you the agency to use your information to launch yourself into your future. This process is safe and secure, much like the airtight domes on Mars!</p>
             </div>
 
             <div className="content has-text-weight-bold has-text-grey is-medium">
-                <p>Try the demo now</p>
+                <p>Get approved using your information: </p>
                 <PrimaryButton href="/select-experience">Apply for a dome-loan</PrimaryButton>
             </div>
             <div className="content has-text-weight-bold">
