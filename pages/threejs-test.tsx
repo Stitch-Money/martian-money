2819f4d1f808c1f607b99596cdc0e2659db373b2
Nick Cuthbert <nick@cuthbert.co.za>
2020-10-30 18:14:32 +0200

    Things

--- pages/threejs-test.tsx
+++ pages/threejs-test.tsx
@@ -1,20 +1,47 @@
 import React, { Suspense, useLayoutEffect, useRef } from "react";
 import { Canvas, ReactThreeFiber, useFrame } from "react-three-fiber";
 import dynamic from 'next/dynamic';
-import THREE from "three";
+import THREE, { Euler, InterpolateSmooth, Vector3 } from "three";
 
 type Keyframe = {
     start: number,
     domContent: (time: number, fracTillNextKeyframe: number) => JSX.Element,
-    threejs: (time: number, fracTillNextKeyframe: number) => void
+    transform?: { position?: Vector3; rotation?: Euler },
+    threejs?: (time: number, fracTillNextKeyframe: number, group: ReactThreeFiber.Object3DNode<THREE.Group, typeof THREE.Group>) => void
 };
 
 const keyframes: Keyframe[] = [{
     start: 0,
-    domContent: (time: number, fracTillNextKeyframe: number) => <div>Frog</div>,
-    threejs: (time: number, fracTillNextKeyframe: number) => { }
+    domContent: (time, fracTillNextKeyframe) => <div>Frog</div>,
+    transform: { position: new Vector3(0, 0, -10) }
+}, {
+    start: 0.25,
+    domContent: (time, fracTillNextKeyframe) => <div>Frog</div>,
+    transform: { position: new Vector3(0, -4, 4) }
+},
+{
+    start: 1,
+    domContent: (time, fracTillNextKeyframe) => <div>Frog</div>,
+    transform: { position: new Vector3(0, 4, 4) }
 }];
 
+if (!keyframes.every((v, i) => i === 0 || keyframes[i - 1].start <= keyframes[i].start)) {
+    throw new Error('Keyframes are not correctly ordered');
+}
+
+
+function getPrevCurrentAndNextCurrentKeyFrame(time: number) {
+    for (let i = 0; i < keyframes.length; ++i) {
+        let keyframe = keyframes[i];
+        if (keyframe.start > time) {
+            let next = keyframe;
+            let current = keyframes[i - 1] ?? null;
+            let prev = keyframes[i - 2] ?? null;
+            return { next, current, prev };
+        }
+    }
+    return { current: keyframes[keyframes.length - 1], next: null, prev: null };
+}
 
 function getDocumentHeight() {
     const body = document.body;
@@ -22,6 +49,12 @@ function getDocumentHeight() {
 
     return Math.max(body.scrollHeight, body.offsetHeight,
         html.clientHeight, html.scrollHeight, html.offsetHeight);
+}
+
+function getTime() {
+    const scrollY = window.scrollY;
+    const height = getDocumentHeight() - window.innerHeight;
+    return scrollY / height;
 }
 
 const MarsScene = dynamic(
@@ -42,7 +75,8 @@ function ThreeJsLanding() {
             const frac = scrollY / height;
             (thisGroup.rotation as any).x = Math.PI * 2 * frac;
         }
-    })
+    });
+
     return <> <perspectiveCamera fov={100} />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
@@ -54,7 +88,6 @@ function ThreeJsLanding() {
 }
 
 export default function ThreeJsTest() {
-
 
     return <div style={{ width: '100vw', height: '500vh', position: 'absolute', padding: 0, margin: 0 }}>
         <div style={{ width: '100vw', height: '500vh', display: 'flex', position: 'absolute' }}>
