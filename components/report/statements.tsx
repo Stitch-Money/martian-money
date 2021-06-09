bdef83e95b30a2f0aa87f091e23599754159ed47
Priyen Pillay <priyen@stitch.money>
2021-06-09 02:23:13 +0200

    safety

--- components/report/statements.tsx
+++ components/report/statements.tsx
@@ -1,4 +1,5 @@
 import { SecondaryButton } from 'components/buttons/secondary-button';
+import { getStitchAccessToken } from 'integrations/storage/session-storage';
 import React from 'react';
 import { Statement } from '../../integrations/stitch/types';
 import { getDateWithShortMonthLabel } from '../../util/income-expense-summary';
@@ -11,7 +12,7 @@ export function StatementLine(statement: Statement) {
                 <span className="has-text-grey has-text-weight-light is-size-7">{ getDateWithShortMonthLabel(new Date(statement.endDate)) }</span>
             </div>
         </div>
-        <SecondaryButton style={{ maxWidth: '60px' }} href={statement.downloadUrl}>⇩</SecondaryButton>
+        <SecondaryButton style={{ maxWidth: '60px' }} onClick={() => downloadStatement(statement.downloadUrl)}>⇩</SecondaryButton>
     </div>;
 }
 
@@ -33,4 +34,27 @@ export default function TopStatementsCard({ statements }: { statements: Statemen
             </div>
         </div>
     );
+}
+
+export function downloadStatement(downloadUrl: string) {
+    const stitchToken = getStitchAccessToken();
+    const authHeader = `Bearer ${stitchToken}`;
+    console.log(authHeader);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", authHeader);
+
+    var requestOptions: RequestInit = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: "no-cors" as RequestMode,
+    redirect: 'follow'
+    };
+
+    fetch(downloadUrl, requestOptions)
+    .then( res => res.blob() )
+    .then( blob => {
+        var file = window.URL.createObjectURL(blob);
+        window.location.assign(file);
+    });
 }
